cc7da7b13a727564ed82aff2599cf1abef6539e8
Unknown <pako-96@o2.pl>
2018-11-22 12:43:39 +0100

    verbose

--- cards/models.py
+++ cards/models.py
@@ -4,5 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Words(models.Model):
-    pl_word = models.CharField(max_length = 100)
-    eng_word = models.CharField(max_length = 100)
+    class Meta:
+        verbose_name = 'Słowo'
+        verbose_name_plural = 'Słówka'
+    
+    pl_word = models.CharField(max_length = 100, verbose_name = "Po polsku")
+    eng_word = models.CharField(max_length = 100, verbose_name = "Po angielsku")
+    
+    def __str__(self):
+        return "{} -> {}".format(self.pl_word, self.eng_word)
+
